61c8353f25bce7f9fe6aa83f76b1a6ca4f7efc37
Sanjana Jonnalagadda <sanjana12j@gmail.com>
2020-11-08 21:15:27 -0500

    Added Material Core Template and Modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import img from "./CountryMap.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    // background: "country-travel-route/src/CountryMap.png"
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const myImg = require("./CountryMap.png");
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Country List Project 
-        </a>
-      </header>
+
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      {/* TODO: Modify image */}
+      {/* <Grid item xs={false} sm={4} md={7} className={classes.image} />  */}
+      <img src={myImg} className={classes.image} alt="countryMap" ></img>
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+
+      <div className={classes.paper}>
+      <Container maxWidth="lg">
+        Country List Map 
+      </Container>
+        <Typography component="h1" variant="h5">
+          Identify All Countries A Driver Needs to Travel From the USA to Their Destinaiton 
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="countryCode"
+            label="Destination Country Code"
+            name="countryCode"
+            autoComplete="countryCode"
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Find Path To Destination
+          </Button>
+        </form>
+      </div>
+    </Grid>
+    </Grid>
     </div>
   );
 }
